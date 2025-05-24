4f01bdb2d0cad2ca0df062787fb6e8149802b9b4
Mihai Neagu <mihai.neagu@emag.ro>
2025-05-24 20:24:13 +0300

    Add menu items

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
-import { useState } from 'react'
+import { MdEmail, MdSubscriptions } from 'react-icons/md'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <h1>Fin - yout financial buddy</h1>
+        <h1>Finn - your financial buddy</h1>
       </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      <div className="menu-container">
+        <div className="menu-grid">
+          <button className="menu-item">
+            <MdEmail className="menu-icon" />
+            <span>Financial Impact Scan</span>
+          </button>
+          
+          <button className="menu-item">
+            <MdSubscriptions className="menu-icon" />
+            <span>Optimize Subscriptions</span>
+          </button>
+        </div>
       </div>
+      
       <p className="read-the-docs">
         Your personal financial companion
       </p>
